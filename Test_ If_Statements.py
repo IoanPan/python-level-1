6cc7d81e5ae8b1e4389a8c4c1805dba8e77bba91
IoanPan <168269795+IoanPan@users.noreply.github.com>
2024-05-04 07:27:25 +0100

    Add files via upload

---
+++ Test_ If_Statements.py
@@ -0,0 +1,11 @@
+#output to user
+answer = input("1) A, 2) B")
+
+#if statements
+if (answer == "1"):
+  say=("How many do you want?")
+  print(say)
+elif(answer == "2"):
+  print("When do you want it?")
+else:
+  print("No option chosen")
