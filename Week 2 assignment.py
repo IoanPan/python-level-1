92359479f534e0d2f608ff2c7670131fc0f5e57e
IoanPan <168269795+IoanPan@users.noreply.github.com>
2024-05-17 22:51:34 +0100

    Add files via upload

---
+++ Week 2 assignment.py
@@ -0,0 +1,13 @@
+#Welcome message
+print("Welcome to the Prime Checker!")
+import math
+num = int(input("Please enter a number:"))
+#if statements
+if num < 2:
+  print("This is not a prime number")
+for i in range(2,num):
+  if (math.sqrt(num))%i==0
+  print(This is a prime number)
+  else:
+    print(This is not a prime number)
+  
